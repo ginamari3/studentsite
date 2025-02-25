169524af71e0e0317f54bb61e9a0dc5ca757d82c
ginamari3 <ginamari@hawaii.edu>
2025-02-24 14:22:11 -1000

    update js

--- art128/base-togglenav/js/script.js
+++ art128/base-togglenav/js/script.js
@@ -1,6 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-    //JAVASCRIPT FUNCTIONS GO HERE
-    console.log('Javascript is ready');
+    //TOGGLE NAV MOBILE MENU FOR SMALL SCREENS
+    const menubutton = document.querySelector('.menu-button');
+    const menunav = document.querySelector('.toggle-nav');
+    menubutton.addEventListener('click', function() {
+        if (menunav.getAttribute('data-navstate')=== 'open') {
+            // if true do this
+            menunav.setAttribute('data-nacstate', 'closed');
+        } else {
+            // else (if false) do this
+            menunav.setAttribute('data-nacstate', 'open');
+        }
+    });
 
 });
